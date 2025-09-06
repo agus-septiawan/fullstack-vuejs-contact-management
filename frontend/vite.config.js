02640a9a0442b904530c37cc750b507cd9b86cf7
agus-septiawan <agusseptiawanasep@gmail.com>
2025-09-06 11:13:23 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: ['*'] // biar semua domain (termasuk Railway) diizinkan
+  }
 })
